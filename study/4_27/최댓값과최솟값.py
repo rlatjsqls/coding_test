ddc44e3a90a22cf0eeb95d5912ac10c886cd1142
rlatjsqls <hobin9697@naver.com>
2023-04-26 17:34:29 +0900

    23/4/26

--- study/4_27/최댓값과최솟값.py
+++ study/4_27/최댓값과최솟값.py
@@ -1,12 +1,5 @@
 def solution(s):
     answer = ""
-    arr = s.split(" ")
-    arr_max = int(arr[0])
-    arr_min = int(arr[0])
-    for i in arr:
-        if int(i) > arr_max:
-            arr_max = int(i)
-        if int(i) < arr_min:
-            arr_min = int(i)
-    answer = str(arr_min) + " " + str(arr_max)
+    arr = list(map(int,s.split(" ")))
+    answer = str(min(arr)) + " " + str(max(arr))
     return answer
